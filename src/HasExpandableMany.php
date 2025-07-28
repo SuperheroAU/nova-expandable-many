34d17f431bd450f68646634797f7b5f334d9b512
Chen Wang <chenggangw@superhero.com.au>
2025-07-28 15:02:14 +1000

    fix signature

--- src/HasExpandableMany.php
+++ src/HasExpandableMany.php
@@ -2,16 +2,12 @@
 
 namespace Lupennat\ExpandableMany;
 
+use Laravel\Nova\Fields\FieldCollection;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
 trait HasExpandableMany
 {
-    /**
-     * Resolve the index fields.
-     *
-     * @return \Laravel\Nova\Fields\FieldCollection<int, \Laravel\Nova\Fields\Field>
-     */
-    public function indexFields(NovaRequest $request)
+    public function indexFields(NovaRequest $request): FieldCollection
     {
         return $this->availableFields($request)
             ->when($request->viaManyToMany(), $this->relatedFieldResolverCallback($request))
